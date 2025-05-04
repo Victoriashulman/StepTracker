ce4c24ccdff209a842a86f45ced134878f7a2631
Victoriashulman <victorias252@nycstudents.net>
2025-05-04 23:52:44 +0000

    commit

--- StepTracker.java
+++ StepTracker.java
@@ -1,1 +1,26 @@
-
+public class StepTracker
+{
+    private int days;
+    private int steps;
+    private int threshhold;
+    private int activeDays;
+    public StepTracker(int t)
+    {
+        threshhold = t;
+    }
+    public int activeDays()
+    {
+        return activeDays;
+    }
+    public double averageSteps()
+    {
+        if (days == 0) return 0;
+        return (double) steps / days;
+    }
+    public void addDailySteps(int s)
+    {
+        days++;
+        steps += s;
+        if (s >= threshhold) activeDays++;
+    }
+}
